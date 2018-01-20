558bf35c4914d8a9aa330840b6e5181316a8cc26
zhangsan <987054131@qq.com>
2018-01-20 11:15:19 +0800

    First commit.

---
+++ app/src/main/java/com/example/administrator/annieweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.annieweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2018/1/18.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/annieweather/db/Province.java
@@ -0,0 +1,10 @@
+package com.example.administrator.annieweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2018/1/19.
+ */
+
+public class Province extends DataSupport {
+}
